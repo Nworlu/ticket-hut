d6e3c3441d5a63225a60bff0c34df90c64657046
ellsurge <iorsasesesugh@gmail.com>
2023-04-24 13:38:01 +0100

    finalised the homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,46 +6,81 @@ import PrimaryButton from '../components/PrimaryButton'
 function HomePage() {
   return (
     <div className='homepage-container'>
-      <h2 className='hero-sectoin'>The very best events</h2>
+      <h2 className='hero-section'>The very best <span className='brand-text'> events</span></h2>
       <div className='homepage-div'>
         <div className='homepage-event'>
-        <div className='homepage-image-container'>
-            <img src={imageBack} />
-        </div>
-        <div className='hompage-event-detail'>
-            <h4>War against drugs</h4>
-            <p>25th April 2023</p>
-            <p><span>By: </span>Computer Science</p>
-            <span>Starting price</span><span> 2500</span>
-        </div>
-        </div>
-        <div className='homepage-event'>
-        <div className='homepage-image-container'>
-            <img src={imageBack} />
-        </div>
-        <div className='hompage-event-detail'>
-            <p>25th - April 2023</p>
-            <p>Computer Science</p>
-        </div>
-        </div>
-        <div className='homepage-event'>
-        <div className='homepage-image-container'>
-            <img src={imageBack} />
-        </div>
-        <div className='hompage-event-detail'>
-            <p>25th - April 2023</p>
-            <p>Computer Science</p>
-        </div>
-        </div>
-        <div className='homepage-event'>
-        <div className='homepage-image-container'>
-            <img src={imageBack} />
-        </div>
-        <div className='hompage-event-detail'>
-            <p>25th - April 2023</p>
-            <p>Computer Science</p>
-        </div>
-        </div>
+
+          <div className='homepage-image-container'>
+              <img src={imageBack} />
+          </div>
+
+          <div className='hompage-event-detail'>
+            <div className='homepage-event-host-price'>
+            <span className='homepage-event-host'>department of natural and a pplied scince </span><span className='homepage-event-price'>2500</span>
+          </div>
+              <h4 className='homepage-event-title'>War against drugs</h4>
+          </div>
+
+            <p className='homepage-event-about'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam</p>
+              <div className='homepage-event-date-time'><span className='date'>April  25th</span> <span className='time'>23:00</span></div>
+
+            </div>
+            <div className='homepage-event'>
+
+<div className='homepage-image-container'>
+    <img src={imageBack} />
+</div>
+
+<div className='hompage-event-detail'>
+  <div className='homepage-event-host-price'>
+  <span className='homepage-event-host'>department of natural and a pplied scince </span><span className='homepage-event-price'>2500</span>
+</div>
+    <h4 className='homepage-event-title'>War against drugs</h4>
+</div>
+
+  <p className='homepage-event-about'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam</p>
+    <div className='homepage-event-date-time'><span className='date'>April  25th</span> <span className='time'>23:00</span></div>
+
+  </div>
+  <div className='homepage-event'>
+
+<div className='homepage-image-container'>
+    <img src={imageBack} />
+</div>
+
+<div className='hompage-event-detail'>
+  <div className='homepage-event-host-price'>
+  <span className='homepage-event-host'>department of natural and a pplied scince </span><span className='homepage-event-price'>2500</span>
+</div>
+    <h4 className='homepage-event-title'>War against drugs</h4>
+</div>
+
+  <p className='homepage-event-about'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam</p>
+    <div className='homepage-event-date-time'><span className='date'>April  25th</span> <span className='time'>23:00</span></div>
+
+  </div>
+  <div className='homepage-event'>
+
+<div className='homepage-image-container'>
+    <img src={imageBack} />
+</div>
+
+<div className='hompage-event-detail'>
+  <div className='homepage-event-host-price'>
+  <span className='homepage-event-host'>department of natural and a pplied scince </span><span className='homepage-event-price'>2500</span>
+</div>
+    <h4 className='homepage-event-title'>War against drugs</h4>
+</div>
+
+  <p className='homepage-event-about'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+numquam</p>
+    <div className='homepage-event-date-time'><span className='date'>April  25th</span> <span className='time'>23:00</span></div>
+
+  </div>    
+
       </div>
       <PrimaryButton style={{marginTop:'50px', width:'320px', marginBottom: '40px'}} >
       Load More
